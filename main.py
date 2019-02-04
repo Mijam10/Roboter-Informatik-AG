1e33d6a3963429bcbf82c1e9e59880ca71ee52da
Jan2121 <46785235+Jan2121@users.noreply.github.com>
2019-02-04 16:49:38 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 #die Klasse des Server ist in Server.main
-from Server.main import BefehlsServer
+from Server.server import ServoServer
 
 #intizalieisiert den Server
-server = BefehlsServer()
+B1 = ServoServer(2255)
+B2 = ServoServer(2256)
+B3 = ServoServer(2257)
+B4 = ServoServer(2258)
 
 #Starte den Server/das Programm laüft weiter wärend der Server an ist
-server.start()
+B1.start()
+B2.start()
+B3.start()
+B4.start()
 
-#Ändert die Geschwindigkeit 0-10 ist nach forne und 0-(-10) nach hinten
-server.setF(-1)
-#änder die Geschwindigkeit zur seite -10 ist nach Links und 10 nach rechts
-server.setS(3)
+#Ändert die Gradzahl am oberen Servo
+B1.setF(-1)
+B2.setF(-3)
+B3.setF(-5)
+B4.setF(-4)
+#änder die Gradzahl am Unteren Servo
+B1.setS(3)
 # Um auf die befehle zu zu greifen muss der Ordenr Serve Eingefügt werden
--- raspberryclient.py
+++ raspberryclient.py
@@ -1,8 +1,11 @@
 import socket
-def getCommand(ip):
+def grad(ip, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((ip, 2255))
+    s.connect((ip, port))
     msg = (s.recv(1024).decode('ascii')).split()
     return msg
-#Dammit kann man vom raspberry den befehl einholen C6H12O6 muss aber mit dem namen des empfängers angegeben werden
-print(getCommand("C6H12O6"))
+#Dammit kann man vom raspberry den befehl einholen C6H12O6 muss aber mit dem namen des empfängers angegeben werden und dem port
+print(grad("C6H12O6", 2255))
+print(grad("C6H12O6", 2256))
+print(grad("C6H12O6", 2257))
+print(grad("C6H12O6", 2258))
