426f1aa9a26e08c538411b3ca8f2345f2ee0f9ca
Jan2121 <46785235+Jan2121@users.noreply.github.com>
2019-02-04 16:51:38 +0100

    Delete raspberryclient.py

--- BefehlsServer/raspberryclient.py
+++
@@ -1,8 +0,0 @@
-import socket
-def getCommand(ip):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((ip, 2255))
-    msg = s.recv(1024).decode('ascii')
-    return msg
-#Dammit kann man vom raspberry den befehl einholen C6H12O6 muss aber mit dem namen des empfängers angegeben werden
-print(getCommand("C6H12O6"))
